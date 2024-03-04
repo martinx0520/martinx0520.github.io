ae7edc6e340fd6e1d105202bf305904700000df9
martinx0520 <121643655+martinxing0520@users.noreply.github.com>
2024-03-04 16:31:46 -0500

    tweaked it for hosting user page

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
